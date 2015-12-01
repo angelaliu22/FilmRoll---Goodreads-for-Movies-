f4859c57a36e9374823afe295e0d4c7b0a5a34b1
Angela C Liu <angelaliu22@gmail.com>
2015-11-30 23:08:02 -0500

    Initial Commit

---
+++ iOS Final Project/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  iOS Final Project
+//
+//  Created by Angela Liu on 11/30/15.
+//  Copyright (c) 2015 Angela Liu. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
